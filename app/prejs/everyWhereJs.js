e281480aa8f4678154c5a89449bda27fe3caaa0c
Adriatika <AdriatikaRich@gmail.com>
2019-03-26 17:44:36 +0700

    add popup form. Styles and script

--- app/prejs/everyWhereJs.js
+++ app/prejs/everyWhereJs.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 var li1 	 = document.querySelector('.sm1'),
 	li2 	 = document.querySelector('.sm2'),
+	li1A	 = li1.querySelector('a'),
+	li2A	 = li2.querySelector('a'),
 	sm1 	 = li1.querySelector('ul'),
 	sm2 	 = li2.querySelector('ul'),
 	count1 	 = 0,
@@ -25,4 +27,47 @@ var li1 	 = document.querySelector('.sm1'),
 			}
 			count2++;
 		};	
+		li2A.onclick = function(e){
+			e.preventDefault();
+		}
+		li1A.onclick = function(e){
+			e.preventDefault();
+		}
+	var upFrom 	= document.querySelector("#pp"),
+		close 	= document.querySelector("#ppCl"),
+		popupW 	= document.querySelector(".popupWrapper"),
+		popupF 	= document.querySelector("#ppForm");
+
+			console.log(typeof(formD)) ; 
+		upFrom.onclick = function(e){
+			e.preventDefault();
+			var formD	= document.querySelector("#sp-form-118170");
+			var spWrap 	= document.querySelector(".sp-form-outer") ;
+			popupF.append(formD);
+			popupW.style.display="block";
+			popupF.style.display="block";  
+		}
+		close.onclick=function(){
+			var formD	= document.querySelector("#sp-form-118170");
+			var spWrap 	= document.querySelector(".sp-form-outer") ;
+			popupW.style.display="none";	
+			popupF.style.display="none";	
+			spWrap.appendChild(formD);
+		}
+		popupW.onclick = function(){
+			var formD	= document.querySelector("#sp-form-118170");
+			var spWrap 	= document.querySelector(".sp-form-outer") ;
+			popupW.style.display="none";
+			popupF.style.display="none";	
+			spWrap.appendChild(formD);		
+		}
+		 $(this).keydown(function(e){
+		 	if(e.which == 27){
+			var formD	= document.querySelector("#sp-form-118170");
+			var spWrap 	= document.querySelector(".sp-form-outer") ;
+              		popupW.style.display="none";
+					popupF.style.display="none";	
+					spWrap.appendChild(formD);
+				}
+     			});
 	});
